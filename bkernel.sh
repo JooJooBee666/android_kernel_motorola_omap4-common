6145211463ef2d35fb51bc266116e9086ac4b38b
JooJooBee666 <desire4homicide@gmail.com>
2016-10-11 08:08:15 -0400

    build: update build script file and add modded libthermal-manager.cfg

--- bkernel.sh
+++ bkernel.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -m
 
-buildHome="/media/oldlinux/Android-Kitchen/cm12.1"
+# Set your Cm13 root build folder here
+buildHome="/media/oldlinux/Android-Kitchen/cm13.0"
+
 echo -ne "Kernel Name: "
 read kname
-outZip="cm-12-Custom-OC-$kname-$(date +"%Y-%m-%d").zip"
+outZip="cm-13-OC-$kname-$(date +"%Y-%m-%d").zip"
 cd $buildHome/
 
 if [ -d "$buildHome/out/target/product/maserati/obj/KERNEL_OBJ" ]; then
@@ -13,7 +15,7 @@ if [ -d "$buildHome/out/target/product/maserati/obj/KERNEL_OBJ" ]; then
   rm $buildHome/out/target/product/maserati/kernel
   rm $buildHome/out/target/product/maserati/boot.img
 fi
-export LOCALVERSION="-JJB666-m-$kname-5.0"
+export LOCALVERSION="-JJB666-$kname-6.0"
 
 printenv
 
@@ -30,20 +32,31 @@ if [ ! -d "$buildHome/out/jjb666" ]; then
 fi
 
 cp -r $buildHome/kernel/motorola/omap4-common/buildfiles/META-INF $buildHome/out/jjb666/
-cp -r /home/MADHOPS/jasonl/Android-Kitchen/6.0stash/kernel-mod/buildfiles/META-INF /media/oldlinux/Android-Kitchen/cm13/out/jjb666/
+
 if [ ! -d "/media/oldlinux/Android-Kitchen/cm13/out/jjb666/system" ]; then
-  mkdir /media/oldlinux/Android-Kitchen/cm13/out/jjb666/system
+  mkdir $buildHome/out/jjb666/system
 fi
 if [ ! -d "/media/oldlinux/Android-Kitchen/cm13/out/jjb666/system/etc" ]; then
-  mkdir /media/oldlinux/Android-Kitchen/cm13/out/jjb666/system/etc
+  mkdir $buildHome/out/jjb666/system/etc
 fi
 if [ ! -d "/media/oldlinux/Android-Kitchen/cm13/out/jjb666/system/etc/kexec" ]; then
-  mkdir /media/oldlinux/Android-Kitchen/cm13/out/jjb666/system/etc/kexec
+  mkdir $buildHome/out/jjb666/system/etc/kexec
 fi
-cp ./out/target/product/maserati/kernel ./out/jjb666/system/etc/kexec
+if [ ! -d "$buildHome/out/jjb666/system/lib" ]; then
+  mkdir $buildHome/out/jjb666/system/lib
+fi
+if [ ! -d "$buildHome/out/jjb666/system/lib/modules" ]; then
+  mkdir $buildHome/out/jjb666/system/lib/modules
+fi
+
+cp -r $buildHome/kernel/motorola/omap4-common/buildfiles/libthermal-manager.cfg $buildHome/out/jjb666/system/bin/
+
+cp $buildHome/out/target/product/maserati/kernel $buildHome/out/jjb666/system/etc/kexec
+cd $buildHome/out/target/product/maserati/system/lib/modules/
+cp *.ko $buildHome/out/jjb666/system/lib/modules/
 cd $buildHome/out/jjb666
 
-printf 'ui_print(" |-*-*-|  CM12 Custom Kernel by JooJooBee666  |-*-*-|");'$'\n' > ./META-INF/com/google/android/updater-script
+printf 'ui_print(" |-*-*-| CM13.0 Custom Kernel by JooJooBee666 |-*-*-|");'$'\n' > ./META-INF/com/google/android/updater-script
 printf 'ui_print("                 Device: Moto OMAP4");'$'\n' >> ./META-INF/com/google/android/updater-script
 printf "ui_print(\"      Build time: %s\");" "$(date)" >> ./META-INF/com/google/android/updater-script
 printf $'\n' >> ./META-INF/com/google/android/updater-script
@@ -63,4 +76,3 @@ cd $buildHome/
 
 echo "Created file: out/$outZip"
 
-# cd /sys/devices/system/cpu/cpu0/cpufreq
